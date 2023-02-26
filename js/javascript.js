091cabb08ffdb0644118dd2a5553b4351e2eb9c3
cannaydinn <canaydinn6@gmail.com>
2023-02-26 23:15:20 +0300

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,70 @@
+let taskDOM = document.querySelector("#task")
+let liveToastBtnDOM = document.querySelector("#liveToastBtn")
+let ulDOM = document.querySelector("#list")
+
+liveToastBtnDOM.addEventListener("click", newElement)
+
+function newElement() {
+    let todo = taskDOM.value.trim()
+    if (todo.length > 0) {
+        addTodoList(todo)
+        deleteItem()
+        liveToastBtnDOM.value = ""
+    }
+    else {
+        $(".error.toast").toast("hide")
+    }
+    e.preventDefault()
+}
+function addTodoList(todo) {
+    let liDOM = document.createElement("li")
+    liDOM.classList.add("Lii")
+    liDOM.innerHTML = `${todo}`
+    let span = document.createElement("span")
+    span.classList.add("close")
+    span.innerHTML = `x`
+
+    $(".success.toast").toast("show")
+
+    checkLi(liDOM)
+
+
+
+
+    liDOM.append(span)
+    ulDOM.appendChild(liDOM)
+
+    //local storage add
+    let object = [todo]
+    let list = JSON.parse(localStorage.getItem("Todos"))
+    if (list) {
+        list.push(object)
+        localStorage.setItem("Todos", JSON.stringify(list))
+    } else {
+        localStorage.setItem("Todos", JSON.stringify(object))
+    }
+}
+
+function deleteItem() {
+    let deleteItem = document.querySelectorAll(".close")
+    if (deleteItem.length > 0) {
+        deleteItem.forEach(function (x) {
+            x.addEventListener("click", function () {
+                x.parentElement.remove()
+
+            })
+        })
+    }
+}
+
+function checkLi(isaretlendi) {
+    isaretlendi.addEventListener("click", function() 
+            {  
+                if(isaretlendi.classList.contains("checked")) {
+                    isaretlendi.classList.remove("checked")
+                    
+                } else {
+                    isaretlendi.classList.add("checked")
+                    
+                } })
+}
